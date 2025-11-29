ad989bd0158d3f31f7987875fe299e082c54b008
dwictor0 <daniel.wdev0@gmail.com>
2025-11-29 16:18:11 -0300

    FEAT: VERIFICA_SE_HPING3_ESTA_INSTALADO

--- script_ping.sh
+++ script_ping.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [[ -z "$1" && "$EUID" -ne 0 ]];
 then 
    echo "SCRIPT - PING SCAN"
@@ -11,4 +12,8 @@ do
 hping3 -S -p 80 -c  1 $1.$host 2> /dev/null | grep "flags=SA" | cut -d " " -f 2 | cut -d "=" -f 2;
 done
 fi 
+if ! command -v hping3 >/dev/null 2>&1; then
+   echo "[!!] O comando 'hping3' não foi encontrado."
+   echo "[!!] Consulte o README para instruçoes de instalacao."
+fi
 
